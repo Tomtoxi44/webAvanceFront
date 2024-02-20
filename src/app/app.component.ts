5078b591801617a9fabba17f89f6f02df45b7327
Tommy <tommy.angibaud@gmail.com>
2024-02-20 20:35:59 +0100

    début de l'ajout de button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/MainComp/NavBar/navbar/navbar.component';
+import { NavbuttonComponent } from './components/MainComp/navbutton/navbutton.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, NavbuttonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'projet_jdr';
 }
+
