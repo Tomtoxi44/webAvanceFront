a2f415bc374744f803b229c456d20df989a74658
Tommy <tommy.angibaud@gmail.com>
2024-02-20 20:35:59 +0100

    début de l'ajout de button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/MainComp/NavBar/navbar/navbar.component';
+import { NavbuttonComponent } from './components/MainComp/navbutton/navbutton.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    NavbarComponent
-  ],
+  imports: [RouterOutlet, NavbarComponent, NavbuttonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'projet_jdr';
 }
+
---
+++ src/app/components/MainComp/navbutton/navbutton.component.ts
@@ -0,0 +1,17 @@
+import { NgIf } from '@angular/common';
+import { Component, Input  } from '@angular/core';
+
+@Component({
+  selector: 'app-navbutton',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './navbutton.component.html',
+  styleUrl: './navbutton.component.css'
+})
+export class NavbuttonComponent {
+  public label: string = 'Button label'
+  @Input()
+  display?: boolean;
+  @Input()
+  displayFalse?: boolean;
+}
